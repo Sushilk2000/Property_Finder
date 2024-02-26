f2c93f80a41bdbd781bb885549cb132e5ab96556
SushilKulkarni <sushilkulkarni232@gmail.com>
2024-02-26 19:18:11 +0530

    update

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -11,7 +11,7 @@ function Header() {
           <Link to={"/"}>
             {" "}
             <img
-              src="public/Designer-removebg-preview.png"
+              src="e:\Users\saura\Desktop\Geekster\Rented Property\Properties\public\Designer-removebg-preview.png"
               className="h-full invert"
             />
           </Link>
